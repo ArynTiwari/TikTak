7cc40a1ab5b6527aa19ee36b781b10c25d49e56b
Aryan <arutiwari500@gmail.com>
2022-09-18 01:01:40 +0530

    Day 1 complete

---
+++ tiktak-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name:'comment',
+    title:'Comments',
+    type:'document',
+    fields:[
+        {
+            name:'postedBy',
+            title:'Posted By',
+            type:'postedBy'
+        },
+        {
+            name:'comment',
+            title:'Comments',
+            type:'string',
+        },
+    ],
+};
